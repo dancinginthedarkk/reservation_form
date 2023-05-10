1a6a4b3f5e18941ecb91e3d3e61c8a250075f2f9
dancinginthedarkk <m.angellina@yandex.ru>
2023-05-10 20:35:48 +0300

    add JSDoc and readme

--- src/errorHandling.js
+++ src/errorHandling.js
@@ -1,8 +1,22 @@
+/**
+ * @module errorHandling
+ */
+
+
 const error = document.querySelector('#error');
+
+/**
+ * Отображает сообщение об ошибке.
+ * @param {HTMLElement} element - элемент, в котором будет отображаться сообщение об ошибке.
+ * @param {string} message - отображаемое сообщение об ошибке.
+ */
 export function showErrorMessage(element, message) {
     element.textContent = message;
 }
 
+/**
+ * Удаляет сообщение об ошибке, устанавливая содержимое элемента ошибки в пустую строку.
+ */
 export function clearErrorMessages() {
     error.textContent = '';
 }
